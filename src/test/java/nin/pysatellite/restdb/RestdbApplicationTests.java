b38527304275410a6c60286aebd8f7d0dc12d992
dmario24 <becky2sawyer@gmail.com>
2024-03-06 11:08:10 +0900

    springboot init
    - https://spring.io/guides/gs/accessing-data-rest

---
+++ src/test/java/nin/pysatellite/restdb/RestdbApplicationTests.java
@@ -0,0 +1,13 @@
+package nin.pysatellite.restdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
